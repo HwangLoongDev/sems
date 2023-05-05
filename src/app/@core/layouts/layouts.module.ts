8bcafef6f3ef00101201352bf16d8c389c5ac4c1
SHBHO\nhsos_longth <nhsos_longth@shb.com.vn>
2023-05-05 10:10:10 +0700

    .sidebar

--- src/app/@core/layouts/layouts.module.ts
+++ src/app/@core/layouts/layouts.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { LayoutsComponent } from './layouts.component';
 import { SharedModule } from 'src/app/@shared/shared.module';
 import { ClassicComponent } from './horizontal/classic/classic.component';
+import { MenuListItemComponent } from './components/menu-list-item/menu-list-item.component';
 
 @NgModule({
-  declarations: [LayoutsComponent, ClassicComponent],
+  declarations: [LayoutsComponent, ClassicComponent, MenuListItemComponent],
   imports: [CommonModule, SharedModule],
   exports: [LayoutsComponent],
   providers: [],
